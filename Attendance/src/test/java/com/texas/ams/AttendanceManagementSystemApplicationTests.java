6339a4acedccc76cd918e06f0cc61cba2bb9f185
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-04-15 09:30:40 +0545

    initals

---
+++ Attendance/src/test/java/com/texas/ams/AttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texas.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
